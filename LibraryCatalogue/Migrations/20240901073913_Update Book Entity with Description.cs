50fce154f1e454caa302f80d7fc1d14fd27d02a4
Emzzy241 <emzzyoluwole@gmail.com>
2024-09-01 13:49:22 +0100

    add all

---
+++ LibraryCatalogue/Migrations/20240901073913_Update Book Entity with Description.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryCatalogue.Solution.Migrations
+{
+    public partial class UpdateBookEntitywithDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Books");
+        }
+    }
+}
---
+++ LibraryCatalogue/Program.cs
@@ -0,0 +1,186 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LibraryCatalogue.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace LibraryCatalogue;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      builder.Services.AddDbContext<LibraryCatalogueContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+      
+      /* New code below!!
+        We set up Identity as a service with the line builder.Services.AddIdentity<ApplicationUser, IdentityRole>(). Notice that we specify <ApplicationUser, IdentityRole> — these are the two models that we're using to designate the user and the role. Just like IdentityUser, IdentityRole is a built-in class to Identity, and it allows us to use the default configurations for roles. We won't be configuring roles beyond the defaults, so we use the built-in IdentityRole class here.
+        We chain two more method calls to the Identity service that we set up: .AddEntityFrameworkStores<LibraryCatalogueContext>() and .AddDefaultTokenProviders();. The first method ensures that the Identity user data is saved via EF Core to our database (as represented by the LibraryCatalogueContext class). The second method sets up Identity's providers for tokens, which are created during password reset or two factor authentication, for example. Note that we won't go over how to implement either of those two things in the coursework, and you are encouraged to look into them on your own.
+        Finally, we configure our web application app to .UseAuthentication() and .UseAuthorization(). Remember two things here:
+
+        Whenever we call a method on our WebApplication app, we are configuring how our application handles HTTP requests (the "pipeline"). We configure the request pipeline by setting up middleware. Middleware is software that we add to our request pipeline that determines how the request should be processed. Each middleware decides whether to do some work, or to pass the request onto the next middleware. To optionally review more about this topic, visit the MS Docs.
+        The order in which we set up the middleware matters! If these methods are called in the wrong order, you may run into unhandled exceptions or issues logging in with Identity. Fortunately, the Microsoft Docs has a list of how middleware should be ordered.
+      */
+
+      // override Identity's default settings by configuring our Identity service
+
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<LibraryCatalogueContext>()
+                .AddDefaultTokenProviders();
+
+        // Default Password settings.
+      /* builder.Services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+
+      */
+
+      // });
+      
+        // Changing the default settings only for development mode... Do not do it for production mode
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+          options.Password.RequireDigit = false;
+          options.Password.RequireLowercase = false;
+          options.Password.RequireNonAlphanumeric = false;
+          options.Password.RequireUppercase = false;
+          options.Password.RequiredLength = 0;
+          options.Password.RequiredUniqueChars = 0;
+
+          /*
+            The configuration above allows us to input a password of a single character to create a new user. Even though the RequiredLength property is 0, we can't actually put in an empty password because we have a validation attribute in place that states that some input for the RegisterViewModel.Password property is required.
+            Keep in mind that the above settings should never be used in a production environment — only during development to make our lives a bit easier.
+            Finally, note that when we change our password requirements in Program.cs, we need to make a corresponding update to our [RegularExpression] validation attribute for the RegisterViewModel.Password property.
+          */
+        });
+
+      WebApplication app = builder.Build();
+
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        /*
+            Purpose of app.UseHsts();
+            When you add app.UseHsts(); to your middleware pipeline:
+
+            Enforces HTTPS: HSTS instructs the browser to always use HTTPS instead of HTTP for all subsequent requests to the web application. Once HSTS is enabled, if a user tries to connect to your website over HTTP, the browser will automatically switch to HTTPS.
+
+            Prevents Protocol Downgrade Attacks: HSTS helps prevent attackers from tricking a user’s browser into using a less secure HTTP connection, which can be exploited for man-in-the-middle (MITM) attacks.
+
+            Instructs the Browser to Cache the HTTPS Policy: The browser caches the HSTS policy for a specified period (the "max-age"), which means that even if a user manually tries to connect via HTTP, the browser will still use HTTPS.
+    */
+    }
+    else
+    {
+        // Don't enable HSTS in development
+        app.UseDeveloperExceptionPage();
+    }
+
+      app.UseRouting();
+
+      // New code below!
+      app.UseAuthentication(); 
+      app.UseAuthorization();
+
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+      app.Run();
+    }
+  }
+
+
+/* using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using LibraryCatalogue.Models;
+using Microsoft.AspNetCore.Identity;
+
+// namespace LibraryCatalogue.Models;
+// public class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+//         builder
+//     }
+// }
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Register the DbContext with dependency injection
+builder.Services.AddDbContext<LibraryCatalogueContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register ASP.NET Core Identity services
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryCatalogueContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    /*
+        Purpose of app.UseHsts();
+        When you add app.UseHsts(); to your middleware pipeline:
+
+        Enforces HTTPS: HSTS instructs the browser to always use HTTPS instead of HTTP for all subsequent requests to the web application. Once HSTS is enabled, if a user tries to connect to your website over HTTP, the browser will automatically switch to HTTPS.
+
+        Prevents Protocol Downgrade Attacks: HSTS helps prevent attackers from tricking a user’s browser into using a less secure HTTP connection, which can be exploited for man-in-the-middle (MITM) attacks.
+
+        Instructs the Browser to Cache the HTTPS Policy: The browser caches the HSTS policy for a specified period (the "max-age"), which means that even if a user manually tries to connect via HTTP, the browser will still use HTTPS.
+   
+}
+else
+{
+    // Don't enable HSTS in development
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); // Add this line to enable authentication services
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+ */
